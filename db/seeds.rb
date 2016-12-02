03e74ff0512073d3da751d9e0e36020ac571fa03
Chay Choong <chaychoong@Chays-MacBook-Air.local>
2016-12-02 18:01:55 +0800

    modified seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,30 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'json'
+
+# Book.destroy_allx
+
+file = File.read("db/data.txt")
+data_hash = JSON.parse(file)
+
+counter = 0
+data_hash.each do |k, v|
+	isbn = v['ISBN'].to_s
+	title = v['title']
+	authors = v['authors'].join("; ")
+	copies = rand(1...20)
+	subject = v['subject']
+	year = v['year']
+	price = rand(2000...10000) / 100.0
+	publisher = v['publisher']
+	keywords = v['keywords']
+	form = rand(1..2) == 1 ? "Hardcover" : "Softcover"
+	Book.create("ISBN" => isbn, "title" => title, "authors" => authors, "copies" => copies, "subject" => subject, "year" => year, "price" => price, "publisher" => publisher, "keywords" => keywords, "format" => form)
+	counter += 1
+	print "\rSeeding #{counter} entries: #{isbn}"
+end
+
+puts "\nSeeding complete"
+
