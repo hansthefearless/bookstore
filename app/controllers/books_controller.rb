fdbd797b6170b18a98eca5f22bb64e3f2a6a962a
hj <hanjie_sng@mymail.sutd.edu.sg>
2016-12-09 17:46:00 +0800

    added search

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -29,9 +29,49 @@ class BooksController < ApplicationController
      redirect_to book_path(@book)
    end
 
+   def search
+     @books ||= find_books
+   end
+
    private
-     def book_params
-       params.require(:book).permit(:ISBN, :title, :authors, :copies, :subject, :year, :price, :publisher, :keywords, :format, :thumbnail)
-     end
+      def book_params
+        params.require(:book).permit(:ISBN, :title, :authors, :copies, :subject, :year, :price, :publisher, :keywords, :format, :thumbnail)
+      end
+
+      def find_books
+        Book.find(:all, :conditions => conditions)
+      end
+
+      def title_conditions
+        ["books.title LIKE ?", "%#{title}%"] unless title.blank?
+      end
+
+      def authors_conditions
+        ["books.authors LIKE ?", "%#{authors}%"] unless authors.blank?
+      end
+
+      def publisher_conditions
+        ["books.publisher LIKE ?", "%#{publisher}%"] unless publisher.blank?
+      end
+
+      def subject_conditions
+        ["products.subject = ?", subject] unless subject.blank?
+      end
+
+      def conditions
+        [conditions_clauses.join(' AND '), *conditions_options]
+      end
+
+      def conditions_clauses
+        conditions_parts.map { |condition| condition.first }
+      end
+
+      def conditions_options
+        conditions_parts.map { |condition| condition[1..-1] }.flatten
+      end
+
+      def conditions_parts
+        private_methods(false).grep(/_conditions$/).map { |m| send(m) }.compact
+      end
 
 end
