2adc48ddcc066fbec8fabfabae55692ebdb1be38
hj <hanjie_sng@mymail.sutd.edu.sg>
2016-12-10 21:44:36 +0800

    added validation

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -3,6 +3,9 @@ class RatingsController < ApplicationController
      @book = Book.find(params[:book_id])
      @opinion = Opinion.find(params[:opinion_id])
 
+     if @opinion.customer_id == rating_params[:customer_id]:
+       redirect_to book_path(@book)
+
      @rating = @opinion.ratings.create(rating_params)
 
      redirect_to book_path(@book)
