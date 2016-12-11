35f5f94f2447173de1f21ddea160525138746fca
hj <hanjie_sng@mymail.sutd.edu.sg>
2016-12-11 14:43:26 +0800

    fixed order show

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -11,7 +11,24 @@ class OrdersController < ApplicationController
      @books = Book.joins("INNER JOIN order_books ON order_books.book_id = books.id").joins(
      "INNER JOIN orders ON orders.id = order_books.order_id").where(
      "orders.id = ?", params[:id])
+
      @order = Order.find(params[:id])
+
+     @book_ids = []
+     @books.each do |book|
+       @book_ids << book[:id]
+     end
+
+     @recommended = Order.joins(
+     "INNER JOIN order_books ON order_books.order_id = orders.id").joins(
+     "INNER JOIN books ON books.id = order_books.book_id").where(
+     customer_id: Customer.joins("INNER JOIN orders ON orders.customer_id = customers.id").joins(
+     "INNER JOIN order_books ON order_books.order_id = orders.id").joins(
+     "INNER JOIN books ON books.id = order_books.book_id").where(
+     "customer_id != ?", session[:customer_id]).where(
+     "books.id IN (?)", @book_ids).select("customer_id").distinct).select(
+     "books.*, order_books.copies, sum(order_books.copies) as sold").where.not(
+     "books.id IN (?)", @book_ids).group("books.id").order("sum(order_books.copies) DESC")
    end
 
    def edit
@@ -30,22 +47,6 @@ class OrdersController < ApplicationController
 
        @order_books.save
      end
-
-     @books = []
-     params[:cart].each do |book|
-       @books << book
-     end
-
-     @recommended = Order.joins(
-     "INNER JOIN order_books ON order_books.order_id = orders.id").joins(
-     "INNER JOIN books ON books.id = order_books.book_id").where(
-     customer_id: Customer.joins("INNER JOIN orders ON orders.customer_id = customers.id").joins(
-     "INNER JOIN order_books ON order_books.order_id = orders.id").joins(
-     "INNER JOIN books ON books.id = order_books.book_id").where(
-     "customer_id != ?", session[:customer_id]).where(
-     "books.id IN (?)", @books).select("customer_id").distinct).select(
-     "books.*, order_books.copies, sum(order_books.copies) as sold").where.not(
-     "books.id IN (?)", @books).group("books.id").order("sum(order_books.copies) DESC")
 
      session[:cart] = nil
      redirect_to order_path(@order)
