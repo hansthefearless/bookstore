9089d7260d9ff094e916105aa982cb6c851d23ad
joel <jang93@gmail.com>
2016-12-12 20:55:46 +0800

    editted many things

--- app/controllers/opinions_controller.rb
+++ app/controllers/opinions_controller.rb
@@ -3,7 +3,11 @@ class OpinionsController < ApplicationController
      @book = Book.find(opinion_params[:book_id])
      puts opinion_params[:book_id]
      @opinion = Opinion.create("customer": Customer.find(opinion_params[:customer_id]), "book": Book.find(opinion_params[:book_id]), "text": opinion_params[:text], "score": opinion_params[:score])
-
+     if !@opinion.errors.full_messages.empty?
+       @opinion.errors.full_messages.each do |msg|
+         flash[:danger] = msg
+       end
+     end
      redirect_to book_path(@book)
    end
 
